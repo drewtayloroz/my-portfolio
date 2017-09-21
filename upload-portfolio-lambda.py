fbbadc0e0df2e71442ba8be8d57228594235cad3
Drew Taylor <drew.taylor@upguard.com>
2017-09-21 14:08:27 -0700

    added mimetypes

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -9,22 +9,38 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-west-1:985130161678:deployPortfolioTopic')
 
+    location = {
+        "bucketName": 'portfoliobuild.drewtaylor.name',
+        "objectKey": 'portfoliobuild.zip'
+    }
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+
+        print "Building portfolio from " + str(location)
+
         s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
         portfolio_bucket = s3.Bucket('portfolio.drewtaylor.name')
-        build_bucket = s3.Bucket('portfoliobuild.drewtaylor.name')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
         print "Job done!"
-        topic.publish(Subject="Portfolio Deploy Successfully", Message="Portfolio deployed succesfully!")
+        topic.publish(Subject="Portfolio Deploy Successful", Message="Portfolio deployed succesfully!")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
         topic.publish(Subject="Portfolio Deploy Failed", Message="Portfolio WAS NOT deployed successfully!")
         raise
