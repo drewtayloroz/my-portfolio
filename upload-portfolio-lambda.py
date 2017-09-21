d2141b21f95e86b5faa3b26c48864ace915af490
Drew Taylor <drew.taylor@upguard.com>
2017-09-21 11:23:54 -0700

    Use SMS to notify when portfolio is deployed

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,20 +2,31 @@ import boto3
 from botocore.client import Config
 import StringIO
 import zipfile
-    
+
+
+
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-west-1:985130161678:deployPortfolioTopic')
 
-    portfolio_bucket = s3.Bucket('portfolio.drewtaylor.name')
-    build_bucket = s3.Bucket('portfoliobuild.drewtaylor.name')
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        portfolio_bucket = s3.Bucket('portfolio.drewtaylor.name')
+        build_bucket = s3.Bucket('portfoliobuild.drewtaylor.name')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm)
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    print "Job done!"
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm)
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        print "Job done!"
+        topic.publish(Subject="Portfolio Deploy Successfully", Message="Portfolio deployed succesfully!")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="Portfolio WAS NOT deployed successfully!")
+        raise
+
     return 'Hello from Lambda'
